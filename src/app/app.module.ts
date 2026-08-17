e18a310fd5e639abfc7d5698565147d23951b9e8
deadfair <deadfair666@gmail.com>
2026-08-17 18:53:46 +0200

    sokjodolog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
+import { SymbologyType } from 'symbology';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     NgxQRCodeModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
